692f745de5835134cad73232ae185f08c9df0557
hninmabalo <hninmabalo@gmail.com>
2022-10-03 22:19:15 -0700

    add: set up the starter

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Square from './Square';
+import Board from './Board';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Tic-Tac-Toe
+        </p>
+    </div>
+  );
+}
+
+export default App;
